e00bd3e6254b0540a034e5cc6ea7dfdd1d61917f
puripuri2100 <puripuri2100@gmail.com>
2023-05-29 01:39:53 +0900

    fix

--- src/get_abbreviation.rs
+++ src/get_abbreviation.rs
@@ -15,11 +15,10 @@ pub struct Abbreviation {
   note: Option<String>,
   /// 記述されている条番号
   article: Article,
-  law_num: String,
 }
 
 /// 法令名の略称を、その定義文から自動で抽出する関数
-pub fn get_law_abbreviation(law_num: &str, article: &Article, text: &str) -> Vec<Abbreviation> {
+pub fn get_law_abbreviation(article: &Article, text: &str) -> Vec<Abbreviation> {
   let mut lst = Vec::new();
   let re = Regex::new(
     r#"（(?P<num>(明治|大正|昭和|平成|令和)(一|二|三|四|五|六|七|八|九|十|〇)+年[^（）、。あ-ん]+第(一|二|三|四|五|六|七|八|九|十|百|千|〇)+号)。?(以下)?((?P<note>[^」）]+)において)?、?「(?P<name>[^」]*(法|令))」という。"#,
@@ -36,14 +35,13 @@ pub fn get_law_abbreviation(law_num: &str, article: &Article, text: &str) -> Vec
         name,
         note,
         article: article.clone(),
-        law_num: law_num.to_string(),
       })
     }
   }
   lst
 }
 
-pub async fn get_law_all_abbreviation(law_num: &str, law_xml: &[u8]) -> Result<Vec<Abbreviation>> {
+pub async fn get_law_all_abbreviation(law_xml: &[u8]) -> Result<Vec<Abbreviation>> {
   let text_lst = xml_to_law_text(law_xml).await?;
   let mut lst = Vec::new();
   let mut text_stream = tokio_stream::iter(text_lst);
@@ -51,7 +49,7 @@ pub async fn get_law_all_abbreviation(law_num: &str, law_xml: &[u8]) -> Result<V
     let contents = law_text.contents;
     match contents {
       LawContents::Text(text) => {
-        let mut l = get_law_abbreviation(law_num, &law_text.article_info, &text);
+        let mut l = get_law_abbreviation(&law_text.article_info, &text);
         lst.append(&mut l);
       }
       LawContents::Table(_) => {
@@ -71,7 +69,7 @@ async fn check_get_law_abbreviation_1() {
     sub_item: None,
     suppl_provision_title: None,
   };
-  let v = get_law_abbreviation("", &a, "附則第七条の二及び第七条の三の規定の適用がないものとした場合における地方交付税法等の一部を改正する法律（平成三十一年法律第五号）第三条の規定による改正前の地方特例交付金等の地方財政の特別措置に関する法律（平成十一年法律第十七号）第八条第一項及び地方税法等の一部を改正する等の法律（平成二十八年法律第十三号。以下イにおいて「平成二十八年地方税法等改正法」という。）第九条の規定による廃止前の地方法人特別税等に関する暫定措置法（平成二十年法律第二十五号）第三十九条の規定により読み替えられた平成二十八年地方税法等改正法附則第三十七条の規定による改正前の地方交付税法第十四条（以下この条において「読替え後の地方交付税法第十四条」という。）");
+  let v = get_law_abbreviation(&a, "附則第七条の二及び第七条の三の規定の適用がないものとした場合における地方交付税法等の一部を改正する法律（平成三十一年法律第五号）第三条の規定による改正前の地方特例交付金等の地方財政の特別措置に関する法律（平成十一年法律第十七号）第八条第一項及び地方税法等の一部を改正する等の法律（平成二十八年法律第十三号。以下イにおいて「平成二十八年地方税法等改正法」という。）第九条の規定による廃止前の地方法人特別税等に関する暫定措置法（平成二十年法律第二十五号）第三十九条の規定により読み替えられた平成二十八年地方税法等改正法附則第三十七条の規定による改正前の地方交付税法第十四条（以下この条において「読替え後の地方交付税法第十四条」という。）");
   assert_eq!(
     v,
     vec![Abbreviation {
@@ -79,7 +77,6 @@ async fn check_get_law_abbreviation_1() {
       name: "平成二十八年地方税法等改正法".to_string(),
       note: Some("イ".to_string()),
       article: a,
-      law_num: "".to_string(),
     },]
   )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use clap::Parser;
 use listup_law::LawData;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::path::Path;
 use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -8,6 +10,7 @@ use tokio_stream::StreamExt;
 use tracing::*;
 
 mod get_abbreviation;
+mod get_law_name;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -20,13 +23,16 @@ struct Args {
   error_output: String,
   /// 略称一覧を出力するJSONファイルへのpath
   #[clap(short, long)]
-  abbreviation_output: String,
+  abbreviation_output: Option<String>,
   /// 法令XMLファイル群が置かれている作業ディレクトリへのpath
-  #[clap(short, long)]
+  #[clap(long)]
   work: String,
   /// 法令ファイルのインデックス情報が書かれたJSONファイルへのpath
-  #[clap(short, long)]
+  #[clap(long)]
   index_file: String,
+  /// 法令ファイルのインデックス情報が書かれたJSONファイルへのpath
+  #[clap(long)]
+  abb_list: String,
 }
 
 async fn init_logger() -> Result<()> {
@@ -35,6 +41,14 @@ async fn init_logger() -> Result<()> {
     .finish();
   tracing::subscriber::set_global_default(subscriber)?;
   Ok(())
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
+pub struct Abb {
+  /// 法令番号
+  num: String,
+  /// 略称
+  abbs: Vec<String>,
 }
 
 #[tokio::main]
@@ -49,40 +63,66 @@ async fn main() -> Result<()> {
   let index_str = String::from_utf8(index_buf)?;
   let index_data = serde_json::from_str::<Vec<LawData>>(&index_str)?;
 
-  let mut index_stream = tokio_stream::iter(index_data);
+  let mut index_stream = tokio_stream::iter(index_data.clone());
 
   let work_dir_path = Path::new(&args.work);
 
-  let mut abbreviation_lst = Vec::new();
+  let mut abbreviation_lst: HashMap<String, Vec<get_abbreviation::Abbreviation>> = HashMap::new();
+
+  let mut law_name_map: HashMap<String, Vec<String>> = HashMap::new();
 
   while let Some(law_data) = index_stream.next().await {
+    law_name_map.insert(law_data.clone().num, vec![law_data.clone().name]);
     info!("[START] get abbreviation: {}", law_data.name);
     let path = law_data.file;
     let file_path = &work_dir_path.join(path);
     let mut xml_file = File::open(&file_path).await?;
     let mut xml_text = Vec::new();
     xml_file.read_to_end(&mut xml_text).await?;
-    let mut l = get_abbreviation::get_law_all_abbreviation(&law_data.num, &xml_text).await?;
-    abbreviation_lst.append(&mut l);
+    let l = get_abbreviation::get_law_all_abbreviation(&xml_text).await?;
+    if !l.is_empty() {
+      abbreviation_lst.insert(law_data.num, l);
+    };
     info!("[END] get abbreviation: {}", law_data.name);
   }
 
-  info!("abbreviation_lst.len() = {}", abbreviation_lst.len());
+  if let Some(abbreviation_path) = &args.abbreviation_output {
+    info!("[START] write abbreviation to file: {}", abbreviation_path);
+    let mut abbreviation_output = File::create(abbreviation_path).await?;
+    let abbreviation_lst_str = serde_json::to_string_pretty(&abbreviation_lst)?;
+    abbreviation_output
+      .write_all(abbreviation_lst_str.as_bytes())
+      .await?;
+    abbreviation_output.write_all(b"\n").await?;
+    info!("[END] write abbreviation to file: {}", abbreviation_path);
+  }
 
-  info!(
-    "[START] write abbreviation to file: {}",
-    &args.abbreviation_output
-  );
-  let mut abbreviation_output = File::create(&args.abbreviation_output).await?;
-  let abbreviation_lst_str = serde_json::to_string_pretty(&abbreviation_lst)?;
-  abbreviation_output
-    .write_all(abbreviation_lst_str.as_bytes())
-    .await?;
-  abbreviation_output.write_all(b"\n").await?;
-  info!(
-    "[END] write abbreviation to file: {}",
-    &args.abbreviation_output
-  );
+  let mut egov_abb_file = File::open(&args.abb_list).await?;
+  let mut egov_abb_buf = Vec::new();
+  egov_abb_file.read_to_end(&mut egov_abb_buf).await?;
+  let egov_abb_str = String::from_utf8(egov_abb_buf)?;
+  let egov_abb_lst = serde_json::from_str::<Vec<Abb>>(&egov_abb_str)?;
+  let mut egov_abb_stream = tokio_stream::iter(egov_abb_lst);
+  while let Some(egov_abb) = egov_abb_stream.next().await {
+    if let Some(lst) = law_name_map.get(&egov_abb.num) {
+      let mut l = egov_abb.clone().abbs;
+      l.push(lst[0].clone()); // lstの中身は正式名称の1つしか入っていないのでこれでよい
+      law_name_map.insert(egov_abb.clone().num, lst.clone());
+    } else {
+      law_name_map.insert(egov_abb.clone().num, egov_abb.clone().abbs);
+    }
+  }
+  let mut index_stream = tokio_stream::iter(index_data.clone());
+  while let Some(law_data) = index_stream.next().await {
+    info!("[START] get law name: {}", law_data.name);
+    let path = law_data.file;
+    let file_path = &work_dir_path.join(path);
+    let mut xml_file = File::open(&file_path).await?;
+    let mut xml_text = Vec::new();
+    xml_file.read_to_end(&mut xml_text).await?;
+    // TODO
+    info!("[END] get law name: {}", law_data.name);
+  }
 
   Ok(())
 }
